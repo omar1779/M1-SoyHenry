6330b1cdaf1dacbc44c79e6aee1eb43bb8c11451
omar1779 <josemyclans@gmail.com>
2022-05-23 18:43:13 -0500

    homework 1 completado

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,12 +2,31 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-
+  // sumatoria n*2^i
+  // 1,1,1,0 4-3-i=3
+  // Por lo tanto: 10101
+  // 1x2^4 + 0x2^3 + 1x2^2 + 0x2^1 + 1x2^0 = 16 + 0 + 4 + 0 + 1 = 21
+  let decimal = 0;
+  for (let i = 0; i < num.length; i++){
+    let posicion = num.length -1 -i;
+    let potencia = 2**i;
+    decimal = decimal +  num[posicion]* potencia;
+  }
+  return decimal
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
-
+  // 5/2 = 1
+  // 2/2 = 0
+  // 1/2 = 1
+  // 0
+  let binario = '';
+  while(num > 0){
+    binario = num % 2 + binario
+    num = Math.floor(num/2)
+  }
+  return binario;
 }
 
 
